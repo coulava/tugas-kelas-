55b15683ea0bfc7db1f78a79bd43a78870718d4e
coulava <coulava_rahmawati_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:56:53 +0700

    Add files via upload

---
+++ Tugas bu pasha/ObyekGuru.java
@@ -0,0 +1,58 @@
+  //Driver Class
+public class ObyekGuru {
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        //membuat obyek
+        Guru buPasha = new Guru();
+        buPasha.id = 350;
+        buPasha.nama = "buPasha";
+        buPasha.mapel = "Produktif";
+        buPasha.alamat = "Malang";
+        System.out.println("Ini data buPasha");
+        System.out.println(buPasha.id);
+        System.out.println( buPasha.nama);
+        System.out.println(buPasha.mapel);
+        System.out.println(buPasha.alamat);
+        Guru buAul =  new Guru();
+        buAul.id = 330;
+        buAul.nama = "buAul";
+        buAul.mapel = "Produktif";
+        buAul.alamat = "Malang";
+        System.out.println("Ini data buAul");
+        System.out.println(buAul.id);
+        System.out.println(buAul.nama);
+        System.out.println(buAul.mapel);
+        System.out.println(buAul.alamat);
+        Guru buFeni = new Guru();
+        buFeni.id = 630;
+        buFeni.nama = "buFeni";
+        buFeni.mapel = "Matematika";
+        buFeni.alamat = "Malang";
+        System.out.println("Ini data buFeni");
+        System.out.println(buFeni.id);
+        System.out.println(buFeni.nama);
+        System.out.println(buFeni.mapel);
+        System.out.println(buFeni.alamat);
+        Guru pakDiaur = new Guru();
+        pakDiaur.id = 430;
+        pakDiaur.nama = "pakDiaur";
+        pakDiaur.mapel = "Fisika";
+        pakDiaur.alamat = "Malang";
+        System.out.println("Ini data pakDiaur");
+        System.out.println(pakDiaur.id);
+        System.out.println(pakDiaur.nama);
+        System.out.println(pakDiaur.mapel);
+        System.out.println(pakDiaur.alamat);
+        Guru pakAndre = new Guru();
+        pakAndre.id = 530;
+        pakAndre.nama = "pakAndre";
+        pakAndre.mapel = "English";
+        pakAndre.alamat = "Amerika";
+        System.out.println("Ini data pakAndre");
+        System.out.println(pakAndre.id);
+        System.out.println(pakAndre.nama);
+        System.out.println(pakAndre.mapel);
+        System.out.println(pakAndre.alamat);
+
+    }
+}
